ae630b9452f67c6cdd9a0206c66db62a99e97e40
terrorbladegodlike <ternovetchii.2002@mail.ru>
2024-08-08 16:32:20 +0300

    Added parallax effect to Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,23 @@
 // Import Images
 import { curve, heroBackground, robot } from "../assets";
-import Button from "./Button";
 
 // Import Components
+import Button from "./Button";
 import Section from "./Section";
 
+// Import icons
+import { BackgroundCircles, BottomLine, Gradient } from "./design/Hero";
+import { heroIcons } from "../constants";
+
+// Import Parallax from React-Parallax
+import { ScrollParallax } from "react-just-parallax";
+
+// Import useRef from React
+import { useRef } from "react";
+
 const Hero = () => {
+  const parallaxRef = useRef(null);
+
   return (
     <Section
       className="pt-[12rem] -mt-[5.25rem]"
@@ -14,7 +26,7 @@ const Hero = () => {
       customPaddings
       id="hero"
     >
-      <div className="container relative">
+      <div className="container relative" ref={parallaxRef}>
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
           <h1 className="h1 mb-6">
             Explore the Possibilities of&nbsp;AI&nbsp;Chatting with {` `}
@@ -49,8 +61,18 @@ const Hero = () => {
                   height={490}
                   alt="robot"
                 />
+                <ScrollParallax isAbsolutelyPositioned>
+                  <ul className="hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl xl:flex">
+                    {heroIcons.map((icon, index) => (
+                      <li className="p-5" key={index}>
+                        <img src={icon} width={24} height={25} alt={icon} />
+                      </li>
+                    ))}
+                  </ul>
+                </ScrollParallax>
               </div>
             </div>
+            <Gradient />
           </div>
           <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
             <img
@@ -61,6 +83,8 @@ const Hero = () => {
               alt="hero"
             />
           </div>
+
+          <BackgroundCircles />
         </div>
       </div>
     </Section>
